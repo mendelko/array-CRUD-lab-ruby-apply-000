e436e6317569426b96e321bb571efa5b24830aee
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-11-02 22:07:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = [1, 2, 3, 4]
-  element = 5
-  array << 5
+  add_element_to_end_of_array(array, element).push(5)
 end
 
 def add_element_to_start_of_array(array, element)
